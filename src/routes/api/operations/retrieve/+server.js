b51bb6a8252668b28a27ca32d5174a6098b04025
Hubert Razack <hubert.razack@weareverse.com>
2023-08-08 23:13:04 -0400

    Delete Prisma model

--- src/routes/api/operations/retrieve/+server.js
+++ src/routes/api/operations/retrieve/+server.js
@@ -1,14 +1,9 @@
-import { db } from '$lib/db.server'
 import { supabase } from '$lib/db.server'
 
 export const POST = async({ request }) => {
   // gather all the parameters
   let req = await request.json();
   const { operationId } = req;
-  // retrieve the operation
-  /*let operation = await db.operation.findUnique({
-    where: {id: parseInt(operationId)}
-  });*/
   let response = await supabase.from('operations').select().eq('id', operationId);
   return new Response(JSON.stringify({ operation: response.data[0] }));
 }
