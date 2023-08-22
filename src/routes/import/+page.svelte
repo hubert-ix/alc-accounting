f217512a112baa8d8c3ca512671edbb2eb254e7c
Hubert Razack <hubert.razack@weareverse.com>
2023-08-22 14:10:51 -0400

    Better date import for operations

--- src/routes/import/+page.svelte
+++ src/routes/import/+page.svelte
@@ -1,8 +1,11 @@
 <script>
   import dayjs from "dayjs";
+  import utc from 'dayjs/plugin/utc.js';
   import Button from "$lib/UI/Button.svelte";
 
   let message = "";
+
+  dayjs.extend(utc);
 
   async function importCategories() {
     //let categories = ['Movie / concert','CD & DVD','Computer software','Bank fees','Restaurant','Restaurant (single)','Photography costs','Advertising & promotion','Courier & postage','Office furniture','Music equipment','TTC','Travel','Office supplies','Computer / print supplies','Legal & accounting','Cell phone','Computer hardware','Internet','Donation','Coaching and courses','Something','Books','Landline','Hydro','Gifts'];
@@ -31,7 +34,7 @@
           amount: op.amount,
           hst: op.hst,
           tip: op.tip,
-          date: dayjs.unix(op.date).format("YYYY-MM-DD")
+          date: dayjs.unix(op.date).utcOffset().format("YYYY-MM-DD")
         };
         await fetch("/api/operations/create", { method: "POST", body: JSON.stringify(data), headers: {'content-type': 'application/json'}});
       }
@@ -44,8 +47,8 @@
 <div class="wrap">
   <!--
   <Button caption="Import categories" on:click={importCategories} />
-  <Button caption="Import operations" on:click={importOperations} />
   -->
+  <Button caption="Import operations" on:click={importOperations} />
 
   <div class="console">
     {message}
